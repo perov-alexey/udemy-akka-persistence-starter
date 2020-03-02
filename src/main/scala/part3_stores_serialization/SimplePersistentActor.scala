3a61fa88ed34c9df3b57817a3eeac32febab71c9
Alexey Perov <perov-alexey@yandex.ru>
2020-03-02 21:59:24 +0300

    Done stores and serialization

---
+++ src/main/scala/part3_stores_serialization/SimplePersistentActor.scala
@@ -0,0 +1,39 @@
+package part3_stores_serialization
+
+import akka.actor.ActorLogging
+import akka.persistence._
+
+class SimplePersistentActor extends PersistentActor with ActorLogging {
+
+    override def persistenceId: String = "simple-persistence-actor"
+
+    var nMessages = 0
+
+    override def receiveCommand: Receive = {
+      case "print" =>
+        log.info(s"I have persisted $nMessages so far")
+      case "snap" =>
+        saveSnapshot(nMessages)
+      case SaveSnapshotSuccess(metadata) =>
+        log.info(s"saveSnapshot successful: $metadata")
+      case SaveSnapshotFailure(_, cause) =>
+        log.warning(s"saveSnapshot failed: $cause")
+      case message =>
+        persist(message) { _ =>
+          log.info(s"Persisting $message")
+          nMessages += 1
+        }
+    }
+
+    override def receiveRecover: Receive = {
+      case RecoveryCompleted =>
+        log.info("Recovery done")
+      case SnapshotOffer(_, payload: Int) =>
+        log.info(s"Recovered snapshot $payload")
+        nMessages = payload
+      case message =>
+        log.info(s"Recovered $message")
+        nMessages += 1
+    }
+
+  }
